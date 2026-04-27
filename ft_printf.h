a73ed825fe0bd07edaebda775fb78fee3d4696d5
May <magrass@student.42heilbronn.de>
2026-04-27 02:00:23 +0200

    ft_printf header file

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/26 21:12:55 by magrass           #+#    #+#             */
+/*   Updated: 2026/04/27 01:00:29 by magrass          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stddef.h>
+
+int		ft_printf(const char *fmt, ...);
+
+size_t	ft_strlen(const char *s);
+
+#endif
