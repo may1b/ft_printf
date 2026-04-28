f9edd02c37e6203c918a608445f3bf50f0b28aef
May <magrass@student.42heilbronn.de>
2026-04-28 09:54:58 +0200

    added  string for correct output for null pointers

--- ft_print.c
+++ ft_print.c
@@ -6,7 +6,7 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 01:53:55 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/27 02:35:27 by magrass          ###   ########.fr       */
+/*   Updated: 2026/04/28 09:19:17 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <unistd.h>
 
 #define NULL_STR "(null)"
+#define NULL_PTR_STR "(nil)"
 
 int	ft_print_string(char *str)
 {
@@ -26,6 +27,6 @@ int	ft_print_string(char *str)
 int	ft_print_ptr(size_t ptr)
 {
 	if (!ptr)
-		return (write(1, NULL_STR, ft_strlen(NULL_STR)));
+		return (write(1, NULL_PTR_STR, ft_strlen(NULL_PTR_STR)));
 	return (ft_print_string(ptr_to_string(ptr)));
 }
